5594a56b8ef6990555ee80395aff9278ae0d446c
chepre1986 <chepre1986@gmail.com>
2021-10-18 21:17:03 +0200

    link - touch up

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+ <h1>
  Weather
- <a href="https://github.com/chepre1986/blossoming_weather_forecast.git" target="_blank">Open-source</a>
-   </div>
+ </h1>
+ <p>
+   Created by Patrycja Szumska. Code {" "}
+ <a href="https://github.com/chepre1986/blossoming_weather_forecast.git" target="_blank" rel="noreferrer">Open-source</a>
+  </p> 
+  </div>
   );
 }
 
