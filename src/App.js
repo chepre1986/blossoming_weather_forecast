b5e50f93aab6ba3b411a667343d5da922b9db011
chepre1986 <chepre1986@gmail.com>
2021-10-19 13:04:34 +0200

    Added form and some colors

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
  <h1>
  🌺 Weather 🌺
  </h1>
- <p>
+ 
+  
+ <form className="search-form">
+ <div className="row">
+   <div className="col-6"> <input type="text" className="form-control" placeholder="Enter city name..." /></div>
+ <div className="col-3"> <input type="button" className="btn btn-outline-secondary" value="Search" /></div>
+  </div> 
+  </form>
+ </div>
+
+  <footer className="links">
    Created by Patrycja Szumska. Code {" "}
  <a href="https://github.com/chepre1986/blossoming_weather_forecast.git" target="_blank" rel="noreferrer">Open-source</a>
-  </p> 
+  </footer> 
   </div>
   );
 }
