fef3f10f7f54f8a5cb9b9895f29d151c7b82c031
chepre1986 <chepre1986@gmail.com>
2021-10-21 15:50:56 +0200

    Forecast and date to the weather added
    all not working yet

--- src/Forecast.js
+++ src/Forecast.js
@@ -4,7 +4,104 @@ import './Forecast.css';
 export default function Forecast(){
     return (
         <div className='forecast'>
-            Hello from Forecast
+           <div className='row'>
+               <div className='col first'>
+               <div className='col day-name first-day'>
+                   Tue
+                   </div>        
+<div className='col'>
+🌧   
+</div>   
+<div className='col forecast-temp'>
+    <span className='col-5 max-temp'>
+       15C  
+    </span>
+    <span className='col-2'>
+    {" "}/{" "}  
+    </span>
+<span className='col-5 min-temp'>
+      4C 
+    </span>    
+</div>   
+               </div>
+               <div className='col second'>
+               <div className='col day-name second-day'>
+                   Wed
+                   </div>        
+<div className='col'>
+☀️      
+</div>   
+<div className='col forecast-temp'>
+    <span className='col-5 max-temp'>
+       15C  
+    </span>
+    <span className='col-2'>
+    {" "}/{" "}  
+    </span>
+<span className='col-5 min-temp'>
+      4C 
+    </span>    
+</div>  
+               </div>
+               <div className='col third'>
+               <div className='col day-name third-day'>
+                   Thu
+                   </div>        
+<div className='col'>
+☁️     
+</div>  
+<div className='col day-name forecast-temp'>
+    
+<span className='col-5 max-temp'>
+       15C  
+    </span>
+    <span className='col-2'>
+    {" "}/{" "}  
+    </span>
+<span className='col-5 min-temp'>
+      4C 
+    </span>     
+</div>                    
+                   </div>
+                   <div className='col fourth'>
+                   <div className='col day-name fourth-day'>
+                       Fri
+                       </div>        
+<div className='col'>
+❄️     
+</div>    
+<div className='col forecast-temp'>
+    <span className='col-5 max-temp'>
+       15C  
+    </span>
+    <span className='col-2'>
+    {" "}/{" "}  
+    </span>
+<span className='col-5 min-temp'>
+      4C 
+    </span>    
+</div>                
+               </div>
+               <div className='col fifth'>
+<div className='col day-name fifth-day'>
+Sat     
+</div>        
+<div className='col'>
+🌈     
+</div>       
+<div className='col forecast-temp'>
+    <span className='col-5 max-temp'>
+       15C  
+    </span>
+    <span className='col-2'>
+       {" "}/{" "} 
+    </span>
+<span className='col-5 min-temp'>
+      4C 
+    </span>    
+</div>   
+               </div>
+           </div>
         </div>
     )
 }
--- src/Weather.js
+++ src/Weather.js
@@ -5,14 +5,16 @@ import './Weather.css';
 export default function Weather(){
 return(<div className='weather'>
     <div className='row'>
-        <div className='col-4'>
+        <div className='col-5'>
         <ul className='main-info left'>
                <li className='icon'>🌤️</li> 
                <li className='hour'>20:45</li> 
+               <li className='date'>May 21st 2021</li> 
+               <li className='weekday'>Monday</li> 
                <li className='city-name'>PARIS</li>
         </ul>
        </div>
-       <div className='col-8'>
+       <div className='col-7'>
         <ul className='main-info right'>
             <li className='temperature'>temperature: 16C</li>
             <li className='humidity'>humidity: 35%</li>
